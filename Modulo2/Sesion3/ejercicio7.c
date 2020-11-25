3db647d729b0f811bce5bbeed95aebc9f63debc5
frandominguez03 <d3vcho@gmail.com>
2020-11-25 11:43:44 +0100

    Corrigiendo sesión 3

--- Modulo2/Sesion3/ejercicio7.c
+++ Modulo2/Sesion3/ejercicio7.c
@@ -19,7 +19,6 @@ int main(int argc, char* argv[]) {
 
     char bg[] = "bg";
     bool segundoPlano = 0;
-    char ruta[] = "/usr/bin/";
     pid_t pid;
     int estado;
     
@@ -33,33 +32,15 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // Concatenamos el nombre del programa
-    sprintf(ruta, "%s%s", ruta, argv[1]);
-
     if(segundoPlano == 1) {
-        printf("Llega");
-        // Creo un array de longitud argc-1 porque el último parámetro es 'bg' y no nos interesa
-        char* ejecucion[argc-1];
-
-        // Lo inicializo y asigno el primer valor a ruta y el último valor a NULL
-        // La ruta debe de ser el primer valor del array porque así lo requiere execv
-        // A su vez, el array debe de terminar en NULL porque así lo requiere execv
-        for(int i=1; i < argc-2; i++)
-            ejecucion[i] = "";
-
-        ejecucion[0] = ruta;
-        ejecucion[argc-2] = NULL;
-
-        // Le asigno todos los parámetros
-        for(int i = 1; i < argc-2; i++) {
-            ejecucion[i] = argv[i+1];
-        }
+        
+        argv[argc-1]=NULL;
 
         if((pid = fork()) < 0) {
             perror("\nError en el fork");
             exit(EXIT_FAILURE);
         } else if(pid == 0) {
-            if(execv(ruta, ejecucion) < 0) {
+            if(execvp(argv[1], &argv[1]) < 0) {
                 perror("Error en execv\n");
                 exit(EXIT_FAILURE);
             }
@@ -68,37 +49,9 @@ int main(int argc, char* argv[]) {
         wait(&estado);        
         
     } else {
-        if(argc == 2) {
-            char* ejecucion[2] = {"", NULL};
-
-            if(execv(ruta, ejecucion) < 0) {
-                perror("Error en execl\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        else {
-            // Creo un array de longitud argc
-            char* ejecucion[argc];
-
-            // Lo inicializo y asigno el primer valor a ruta y el último valor a NULL
-            // La ruta debe de ser el primer valor del array porque así lo requiere execv
-            // A su vez, el array debe de terminar en NULL porque así lo requiere execv
-            for(int i=1; i < argc-1; i++)
-                ejecucion[i] = "";
-            
-            ejecucion[0] = ruta;
-            ejecucion[argc-1] = NULL;
-            
-            // Le asigno todos los parámetros
-            for(int i = 1; i < argc-1; i++) {
-                ejecucion[i] = argv[i+1];
-            }
-
-            if(execv(ruta, ejecucion) < 0) {
-                perror("Error en execv %d\n");
-                exit(EXIT_FAILURE);
-            }
+        if(execvp(argv[1], &argv[1]) < 0) {
+            perror("Error en execv %d\n");
+            exit(EXIT_FAILURE);
         }
 
         wait(&estado);
